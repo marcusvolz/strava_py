6c0b98a40a688e2f089f985dcbe4b6fb71419d0a
wiredfool <eric-github@soroos.net>
2023-08-26 12:32:15 +0100

    Initial take on activities.csv for cyclemeter

--- src/stravavis/process_activities.py
+++ src/stravavis/process_activities.py
@@ -1,10 +1,34 @@
 import pandas as pd
-
+from .plot_calendar import ACTIVITY_FORMAT
 
 def process_activities(activities_path):
     # Import activities.csv from Strava bulk export zip
     activities = pd.read_csv(activities_path)
 
     # Further processing (to come)
+
+    return activities
+
+
+
+def process_sqlite_activities(path):
+    import sqlite3
+
+    with sqlite3.connect(path) as con:
+        activities = pd.read_sql_query('''
+            select
+              runID,
+              startTime as "Activity Date",
+              startTimeZone,
+              distance as "Distance",
+              runTime as "Elapsed Time"
+            from run
+        ''', con)
+
+    activities['Activity Date'] = pd.to_datetime(activities['Activity Date'])
+    for (ix, row) in activities.iterrows():
+        activities.loc[ix, ['Activity Date']] = row['Activity Date'].tz_localize(row['startTimeZone'])
+
+    activities['Activity Date'] = pd.to_datetime(activities['Activity Date'], utc=True).dt.strftime(date_format=ACTIVITY_FORMAT)
 
     return activities
